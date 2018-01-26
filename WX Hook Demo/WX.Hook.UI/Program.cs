f0d2adc9f2a2ba946bb334c55803c83e5b2fa359
unknown <bdnacn@Uruguay.bdnacn.com>
2018-01-26 11:01:45 +0800

    add whole error handle

--- WX Hook Demo/WX.Hook.UI/Program.cs
+++ WX Hook Demo/WX.Hook.UI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +15,29 @@ namespace WX.Hook.UI
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            //AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
+            Application.ThreadException += Application_ThreadException;
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmDemo());
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            LogHelper.WXLogger.WXHOOKUI.Error("CurrentDomain_UnhandledException: ", (Exception)e.ExceptionObject);
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            LogHelper.WXLogger.WXHOOKUI.Error("Application.ThreadException: ", e.Exception);
+
+        }
+
+        static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            //Environment.Exit(0);
         }
     }
 }
